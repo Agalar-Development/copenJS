fd52fe0ddf6d6bdb372eca1d515ecc48bdf29b22
CrawLeyYou <60201017+CrawLeyYou@users.noreply.github.com>
2023-11-14 16:31:11 +0300

    Push current Server version

--- src/libs/thread.mjs
+++ src/libs/thread.mjs
@@ -6,44 +6,54 @@ const currentThread = process.argv[2]
 
 console.log("Thread " + currentThread + " started")
 process.on("message", (data) => {
-    var ip = data.ip
     var time = data.time
-     try {
-        protocol.GetServerData(ip.toString(), 25565).then(async (data) => {
+    switch (data.mode) {
+        case "ping":
+            process.send({status: "ping", thread: currentThread, time: time })
+            break;
+        case "search":
+            var ip = data.ip
             try {
-                process.send({ ip: ip.toString(), status: "success", thread: currentThread, time: time})
-                if (data.players.online > 0 ) await webhook(ip.toString(), data.version.name, await protocol.ProtocolTOVersion(data.version.protocol), "Full motd data will be in released with database. ", data.latency, "https://minecraft.global/images/icons/default_favicon.png", new Date().toISOString(), data.players.max, data.players.online, ((data.modinfo?.type ?? false) === "FML") ? true : false)
-                await Database.MongoLogger({
-                    IP: ip.toString(),
-                    Version: data.version.name,
-                    Protocol: data.version.protocol,
-                    ProtocolVersion: await protocol.ProtocolTOVersion(data.version.protocol),
-                    Motd: data.description,
-                    Latency: data.latency,
-                    Favicon: data.favicon,
-                    Timestamp: new Date().toISOString(),
-                    MaxPlayer: data.players.max,
-                    OnlinePlayer: data.players.online,
-                    Players: data.players.sample,
-                    Modinfo: data.modinfo
-                }, "Servers")
+                protocol.GetServerData(ip.toString(), 25565).then(async (data) => {
+                    try {
+                        process.send({ ip: ip.toString(), status: "success", thread: currentThread, time: time })
+                        if (data.players.online > 0) await webhook(ip.toString(), data.version.name, await protocol.ProtocolTOVersion(data.version.protocol), "Full motd data will be in released with database. ", data.latency, "https://minecraft.global/images/icons/default_favicon.png", new Date().toISOString(), data.players.max, data.players.online, ((data.modinfo?.type ?? false) === "FML") ? true : false)
+                        Database.MongoLogger({
+                            IP: ip.toString(),
+                            Version: data.version.name,
+                            Protocol: data.version.protocol,
+                            ProtocolVersion: await protocol.ProtocolTOVersion(data.version.protocol),
+                            Motd: data.description,
+                            Latency: data.latency,
+                            Favicon: data.favicon,
+                            Timestamp: new Date().toISOString(),
+                            MaxPlayer: data.players.max,
+                            OnlinePlayer: data.players.online,
+                            Players: data.players.sample,
+                            Modinfo: data.modinfo
+                        }, "Servers")
+                    }
+                    catch (err) {
+                        //    console.log(err)
+                        //    console.log("This servers data is compromised IP: " + ip.toString())
+                        Database.MongoLogger({
+                            IP: ip.toString(),
+                            Timestamp: new Date().toISOString()
+                        }, "Compromised-Servers")
+                        process.send({ ip: ip.toString(), status: "success", thread: currentThread, time: time })
+                    }
+                }).catch((err) => {
+                    //console.log(err)
+                    process.send({ ip: ip.toString(), status: "error", thread: currentThread, time: time })
+                })
+            } catch (err) {
+                console.log("An unknown error occured")
+                //console.log(err)
+                process.send({ ip: ip.toString(), status: "error", thread: currentThread, time: time })
             }
-            catch (err) {
-                console.log(err)
-                console.log("This servers data is compromised IP: " + ip.toString())
-                await Database.MongoLogger({
-                    IP: ip.toString(),
-                    Timestamp: new Date().toISOString()
-                }, "Compromised-Servers")
-                process.send({ ip: ip.toString(), status: "success", thread: currentThread, time: time})
-            }
-        }).catch((err) => {
-            process.send({ ip: ip.toString(), status: "error", thread: currentThread, time: time})
-        })
-    } catch (err) {
-        console.log("An unknown error occured")
-        console.log(err)
-        process.send({ ip: ip.toString(), status: "error", thread: currentThread, time: time})
+            break;
+        default:
+            return 0
     }
 })
 
--- src/scanner.mjs
+++ src/scanner.mjs
@@ -45,14 +45,18 @@ const getColor = (data, mode) => {
                 return colors[1]
             }
         case "status":
-            if (data === 0) {
-                return colors[2] + "Idle"
-            }
-            else if (data >= 1) {
-                return colors[1] + "Working"
-            }
-            else {
+            if (data.extra > 5) {
                 return colors[0] + "Error"
+            } else {
+                if (data.data === 0) {
+                    return colors[2] + "Idle"
+                }
+                else if (data.data >= 1) {
+                    return colors[1] + "Working"
+                }
+                else {
+                    return colors[0] + "Error"
+                }
             }
         default:
             return null
@@ -65,12 +69,12 @@ const Main = async () => {
         console.log("There is " + os.availableParallelism() + " thread available on this server.")
         currData.subprocesses = os.availableParallelism()
         for (var i = 0; i < os.availableParallelism(); i++) {
-            processList.push(cprocess.fork("./libs/thread.mjs", [i]))
-            logger.debug("Process: " + processList[i].pid + " started")
-            assignedList.push([i, { currentIp: "", lastresponsetext: 0, status: "Idle", assigned: 0, finds: 0, total: 0 }])
+            processList.push([cprocess.fork("./libs/thread.mjs", [i]), []])
+            logger.debug("Process: " + processList[i][0].pid + " started")
+            assignedList.push([i, { currentIp: "", lastresponsetext: 0, status: "Idle", assigned: 0, finds: 0, total: 0, currentPings: 0 }])
         }
         for (var i = 0; i < processList.length; i++) {
-            processList[i].on("message", (data) => {
+            processList[i][0].on("message", (data) => {
                 var x = assignedList.findIndex((sarray) => {
                     const [number] = sarray
                     return number === parseInt(data.thread)
@@ -81,24 +85,30 @@ const Main = async () => {
                     assignedList[x][1].finds++
                     assignedList[x][1].assigned--
                     assignedList[x][1].lastresponsetext = parseInt(Date.now()) - data.time
-                    logger.debug(processList[assignedList[x][0]].pid + " processed data from ip: " + assignedList[x][1].currentIp);
+                    assignedList[x][1].currentIp = data.ip
+                    logger.debug(processList[assignedList[x][0]][0].pid + " processed data from ip: " + assignedList[x][1].currentIp);
                 }
                 else if (data.status === "error") {
                     currData.assigned--
                     assignedList[x][1].assigned--
                     assignedList[x][1].lastresponsetext = parseInt(Date.now()) - data.time
-                    logger.debug(processList[assignedList[x][0]].pid + " failed to processing ip: " + assignedList[x][1].currentIp);
+                    assignedList[x][1].currentIp = data.ip
+                    logger.debug(processList[assignedList[x][0]][0].pid + " failed to processing ip: " + assignedList[x][1].currentIp);
+                }
+                else if (data.status === "ping") {
+                    assignedList[x][1].currentPings = 0
+                    logger.info(processList[assignedList[x][0]][0].pid + " responded to ping request.")
                 }
             })
         }
         console.log("Parser will be started in 3 seconds...")
         setTimeout(() => {
-            // startParser()
+            //startParser('java -Xmx5G -jar ' + process.cwd() + '/libs/copenJSParser-1.0-SNAPSHOT-5ms.jar')
             setInterval(async () => {
                 assignedList.sort((a, b) => { return a[0] - b[0] })
                 let defaultText = `Main Process: \n   Current IP/s: ${currData.total - currData.totalLast} IP/s \n   Total Assigned: ${currData.assigned} \n   Total TCP Restarts: ${currData.tcpRestarts} \n   Total Finds & Total Try: ${currData.finds} & ${currData.total} \n   Current Rate: ${((currData.finds / currData.total) * 100).toFixed(2)}% \n   Total Sub-Processes: ${currData.subprocesses} \n   Current IP: ${currData.lastIp} \nSub-Processes: `
                 assignedList.forEach(async (data, n) => {
-                    defaultText += "\n   Process processNumber: Current IP: currentIP Last Response: lastResponseText Status: statusText Total Assigned: assignedInt Total Finds: findInt ".replace("processNumber", n.toString()).replace("findInt", data[1].finds).replace("assignedInt", data[1].assigned).replace("currentIP", data[1].currentIp).replace("lastResponseText", `${getColor(data[1].lastresponsetext, "ms")}${data[1].lastresponsetext}ms\x1b[0m`).replace("statusText", `${getColor(data[1].assigned, "status")}\x1b[0m`)
+                    defaultText += "\n   Process processNumber: Current IP: currentIP Last Response: lastResponseText Status: statusText Total Assigned: assignedInt Total Finds: findInt ".replace("processNumber", n.toString()).replace("findInt", data[1].finds).replace("assignedInt", data[1].assigned).replace("currentIP", data[1].currentIp).replace("lastResponseText", `${getColor(data[1].lastresponsetext, "ms")}${data[1].lastresponsetext}ms\x1b[0m`).replace("statusText", `${getColor({ data: data[1].assigned, extra: data[1].currentPings }, "status")}\x1b[0m`)
                 })
                 text.text = defaultText
                 currData.totalLast = currData.total
@@ -106,38 +116,60 @@ const Main = async () => {
             setTimeout(() => {
                 text.start()
             }, 1000)
+            setInterval(async () => {
+                for (var i = 0; i < processList.length; i++) {
+                    if (assignedList[i][1].currentPings < 6) {
+                        assignedList[i][1].currentPings++
+                        processList[assignedList[i][0]][0].send({ mode: "ping", time: Date.now() })
+                    }
+                    else {
+                        logger.warn(processList[assignedList[i][0]][0].pid + " didnt responded ping requests for an entire minute.")
+                    }
+                }
+            }, 10000)
         }, 3000)
     })
     server.on("connection", function (socket) {
         socket.on('error', function (err) {
             console.log("Client lost connection")
             logger.warn("The client lost the connection " + err)
+            startParser('java -Xmx5G -jar ' + process.cwd() + '/libs/copenJSParser-1.0-SNAPSHOT-5ms.jar "' + currData.lastIp + '"')
         })
-        socket.on('end', function (data) {
+        socket.on('end', function () {
             console.log("Client disconnected")
-            logger.warn("The client end " + err)
+            logger.warn("The client end ")
         })
         socket.on("data", async function (ip) {
             assignedList.sort((a, b) => { return a[1].assigned - b[1].assigned })
-            processList[assignedList[0][0]].send({ ip: ip.toString(), time: Date.now() })
-            logger.debug(processList[assignedList[0][0]].pid + " assigned to " + ip.toString());
-            assignedList[0][1].currentIp = ip.toString()
-            assignedList[0][1].assigned++
-            assignedList[0][1].total++
-            currData.lastIp = ip.toString()
-            currData.total++
-            currData.assigned++
+            var x = 0
+            while (assignedList[x][1]?.currentPings > 5 && x < os.availableParallelism()) {
+                x++
+            }
+            if (x === os.availableParallelism()) {
+                logger.fatal("All Processes are down!")
+            }
+            else {
+                processList[assignedList[x][0]][0].send({ mode: "search", ip: ip.toString(), time: Date.now() })
+                logger.debug(processList[assignedList[x][0]][0].pid + " assigned to " + ip.toString());
+                assignedList[x][1].assigned++
+                assignedList[x][1].total++
+                currData.lastIp = ip.toString()
+                currData.total++
+                currData.assigned++
+            }
         })
     })
 }
 
-const startParser = async () => {
-    exec('java -Xmx4096m -jar ' + process.cwd() + '/libs/copenJSParser-1.0-SNAPSHOT-5ms.jar', (error, stderr) => {
+const startParser = async (startup) => {
+    exec(startup, (error, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
+            logger.error("exec error: " + error)
             return;
         }
         console.error(`stderr: ${stderr}`);
+        logger.error("stderr: " + stderr)
     })
 }
 
