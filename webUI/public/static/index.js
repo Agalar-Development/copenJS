f1d1cceab55dc2304bc0221b39f85396c91f265d
CrawLeyYou <60201017+CrawLeyYou@users.noreply.github.com>
2024-01-01 21:39:06 +0300

    add

--- webUI/public/static/index.js
+++ webUI/public/static/index.js
@@ -5,13 +5,22 @@ $.ajax({
         socket = new WebSocket(`ws://${document.location.host}:${JSON.parse(atob(result)).wsp}`);
         socket.onopen = function (e) {
             socket.send("client")
-            test()
+            receive()
         }
     }
 })
 
-const test = () => {
+const receive = () => {
     socket.onmessage = async (res) => {
-        console.log(res.data)
+        var socketData = JSON.parse(res.data)
+        if (socketData.mode === "Masscan") {
+            var data = socketData.data.replace(/\s/g, '').replace("\n", "").split(",")
+            document.getElementById("statusText").innerText = "Online"
+            document.getElementById("statusText").style.color = "#00AF12"
+            document.getElementById("mode").innerText = socketData.mode
+            document.getElementById("rate").innerText = data[0].split(":")[1]
+            document.getElementById("done").innerText = data[1].split("done")[0]
+            document.getElementById("found").innerText = data[3].split("found=")[1]
+        }
     }
 }
