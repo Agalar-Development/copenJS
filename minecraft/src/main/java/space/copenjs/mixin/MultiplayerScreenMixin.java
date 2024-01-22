520d0ec919d346bc116b3259ca664dcf9a550b2c
CrawLeyYou <60201017+CrawLeyYou@users.noreply.github.com>
2024-01-22 21:08:11 +0300

    Add copenJS Button to top left of Multiplayer Screen

---
+++ minecraft/src/main/java/space/copenjs/mixin/MultiplayerScreenMixin.java
@@ -0,0 +1,33 @@
+package space.copenjs.mixin;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.tooltip.Tooltip;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import space.copenjs.copenJS;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(MultiplayerScreen.class)
+public abstract class MultiplayerScreenMixin extends Screen {
+	protected MultiplayerScreenMixin(Text title) {
+		super(title);
+	}
+
+	public ButtonWidget copenJSButton = new ButtonWidget.Builder(Text.literal("copenJS"), button -> {
+		copenJS.LOGGER.info("Clicked Button");
+	})
+			.position(10, 7)
+			.size(60, 20)
+			.tooltip(Tooltip.of(Text.literal("Opens copenJS GUI")))
+			.build();
+
+	@Inject(at = @At("TAIL"), method = "init")
+	private void init(CallbackInfo info) {
+		addDrawableChild(copenJSButton);
+	}
+}
