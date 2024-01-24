d7bc41db7397c6ed9114ec4cf07dc14c63ae9ddb
CrawLeyYou <60201017+CrawLeyYou@users.noreply.github.com>
2024-01-24 06:00:02 +0300

    Fix build

--- minecraft/src/main/java/space/copenjs/mixin/MultiplayerScreenMixin.java
+++ minecraft/src/main/java/space/copenjs/mixin/MultiplayerScreenMixin.java
@@ -5,13 +5,12 @@ import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import space.copenjs.helpers.DatabaseHelper;
 import space.copenjs.copenJS;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 
 @Mixin(MultiplayerScreen.class)
 public abstract class MultiplayerScreenMixin extends Screen {
